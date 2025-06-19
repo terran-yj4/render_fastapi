77c801511cb9410f5c4aaf7e73c3aff10afabc80
よう <112384864+terran-yj4@users.noreply.github.com>
2025-06-19 21:11:01 +0900

    add index and present rooting

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Yo's HTML</title>
+        </head>
+        <body>
+            <h1>Hello TDU!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
@@ -31,3 +44,7 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+@app.post("/present")
+async def give_present(present):
+    return {"response": f"サーバーです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
